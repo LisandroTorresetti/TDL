23c04889a67b2d1f52097b36f2cdfb0bd50e33e6
ignacio lugones <ignacio.lugones@wibson.org>
2023-06-08 03:46:37 -0300

    add code for buttons cosito

--- main.go
+++ main.go
@@ -19,11 +19,17 @@ const token string = "fillWithToken"
 var bot *bt.Bot
 
 type Data struct {
-	Pepe int `json:"pepe"`
-	Id   int `json:"id"`
+	NewsWanted []string `json:"news_wanted"`
+	BlackList  []string `json:"black_list"`
+	Id         int      `json:"id"`
 }
 
 type DeleteDataInformation struct {
+	id       int
+	toAnswer int
+}
+
+type GetInformation struct {
 	id       int
 	toAnswer int
 }
@@ -41,6 +47,23 @@ func deleteData(c chan DeleteDataInformation, database db.DB[Data]) {
 	}
 }
 
+func getWhitelist(c chan GetInformation, database db.DB[Data]) {
+	for {
+		toRemove := <-c
+		fmt.Printf("get whitelist was called with id: %d\n", toRemove)
+		data, _ := database.Get(toRemove.id)
+		bot.SendMessage(toRemove.toAnswer, fmt.Sprintf("your data whitelisted is: %v, press /start to see menu again", data.NewsWanted), "", 0, false, false)
+	}
+}
+
+func getBlacklist(c chan GetInformation, database db.DB[Data]) {
+	for {
+		toRemove := <-c
+		fmt.Printf("get blacklist was called with id: %d\n", toRemove)
+		data, _ := database.Get(toRemove.id)
+		bot.SendMessage(toRemove.toAnswer, fmt.Sprintf("your data blacklisted is: %v, press /start to see menu again", data.BlackList), "", 0, false, false)
+	}
+}
 func main() {
 	up := cfg.DefaultUpdateConfigs()
 
@@ -63,6 +86,8 @@ func main() {
 		syscall.SIGQUIT,
 	)
 	deleteChan := make(chan DeleteDataInformation, 100)
+	wi := make(chan GetInformation, 100)
+	bi := make(chan GetInformation, 100)
 	if err == nil {
 		err = bot.Run()
 		if err == nil {
@@ -70,9 +95,10 @@ func main() {
 			if err != nil {
 				log.Printf("error while creating db %v", err)
 			}
-			fmt.Println("entering")
-			start(d, deleteChan)
+			start(d, deleteChan, wi, bi)
 			go deleteData(deleteChan, d)
+			go getBlacklist(bi, d)
+			go getWhitelist(wi, d)
 			fmt.Println("waiting for sigterm")
 			<-c
 			fmt.Println("exiting bot")
@@ -84,9 +110,9 @@ func main() {
 	}
 
 }
-func start(d db.DB[Data], dc chan DeleteDataInformation) {
+func start(d db.DB[Data], dc chan DeleteDataInformation, wi, bi chan GetInformation) {
 	bot.AddHandler("/hi", func(u *objs.Update) {
-		fmt.Println("hi was clicked")
+		fmt.Println("hi was called")
 		bot.SendMessage(u.Message.Chat.Id, "hello", "", u.Message.MessageId, false, false)
 	}, "private")
 
@@ -102,15 +128,32 @@ func start(d db.DB[Data], dc chan DeleteDataInformation) {
 			bot.SendMessage(u.Message.Chat.Id, "your data is being removed", "", u.Message.MessageId, false, false)
 			dc <- toRemove
 		})
+		kb.AddCallbackButtonHandler("Check what was whitelisted", "/hi1", 3, func(update *objs.Update) {
+			toRemove := GetInformation{
+				id:       u.Message.From.Id,
+				toAnswer: u.Message.Chat.Id,
+			}
+			fmt.Println("delete2 was clicked")
+			wi <- toRemove
+		})
+		kb.AddCallbackButtonHandler("Check what was blacklisted", "/hi2", 4, func(update *objs.Update) {
+			fmt.Println("delete 3was clicked")
+			toRemove := GetInformation{
+				id:       u.Message.From.Id,
+				toAnswer: u.Message.Chat.Id,
+			}
+			bi <- toRemove
+		})
 		di := Data{
-			Pepe: 23,
-			Id:   u.Message.From.Id,
+			BlackList:  []string{},
+			NewsWanted: []string{},
+			Id:         u.Message.From.Id,
 		}
 
 		d.Insert(di)
 
 		//Sends the message to the chat that the message has been received from. The message will be a reply to the received message.
-		_, err := bot.AdvancedMode().ASendMessage(u.Message.Chat.Id, "hi to you too 4", "", u.Message.MessageId, false, false, nil, false, false, kb)
+		_, err := bot.AdvancedMode().ASendMessage(u.Message.Chat.Id, "Please select one of the options below.", "", u.Message.MessageId, false, false, nil, false, false, kb)
 		if err != nil {
 			fmt.Printf("error happened, %v\n", err)
 		}
