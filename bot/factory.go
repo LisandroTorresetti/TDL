ec64080bb680ea86d8d9f8240edab1592fdcd3b3
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-06-24 13:38:21 -0300

    Add more services to initialize in factory: the main idea is that here we initialize all the dependencies that NewsBot need. It's like we have two steps to run our bot: 1. Get configs, initialize dependencies, etc 2. Run bot

--- bot/factory.go
+++ bot/factory.go
@@ -1,21 +1,66 @@
 package bot
 
 import (
+	"bot-telegram/db"
 	"bot-telegram/services/gpt"
 	gptConfigLoader "bot-telegram/services/gpt/config"
 	"fmt"
+	teleBot "github.com/SakoDroid/telego"
+	telegramConfig "github.com/SakoDroid/telego/configs"
+	env "github.com/joho/godotenv"
 	"net/http"
+	"os"
 	"time"
 )
 
-const timeout = 1
+const (
+	botTokenEnv = "TELEGRAM_BOT_TOKEN"
+	timeout     = 1
+)
 
-type NewsProvider interface {
-	Summarize(string) (string, error)
+// ToDo: I Dont like this, refactor
+type Data struct {
+	NewsWanted []string `json:"news_wanted"`
+	BlackList  []string `json:"black_list"`
+	Id         int      `json:"id"`
 }
 
-// CreateNewsBot initialize the news bot with all the services that it needs
-func CreateNewsBot() (NewsProvider, error) {
+func (d Data) GetPrimaryKey() int {
+	return d.Id
+}
+
+// CreateNewsBot returns a NewsProvider with all the services it requires initialized
+func CreateNewsBot() (*NewsBot, error) {
+	database, err := db.CreateDB[Data]("postgres", "pepe")
+	if err != nil {
+		return nil, fmt.Errorf("error creating DB: %v", err)
+	}
+
+	err = env.Load()
+	if err != nil {
+		fmt.Println("error loading environment variables")
+		return nil, err
+	}
+
+	token := os.Getenv(botTokenEnv)
+	if token == "" {
+		return nil, fmt.Errorf("error bot token missing")
+	}
+
+	updateConfiguration := telegramConfig.DefaultUpdateConfigs()
+	botConfig := telegramConfig.BotConfigs{
+		BotAPI: telegramConfig.DefaultBotAPI,
+		APIKey: token, UpdateConfigs: updateConfiguration,
+		Webhook:        false,
+		LogFileAddress: telegramConfig.DefaultLogFile,
+	}
+
+	bot, err := teleBot.NewBot(&botConfig)
+	if err != nil {
+		fmt.Println("error creating telegram bot: %v", err)
+		return nil, err
+	}
+
 	gptConfig, err := gptConfigLoader.LoadConfig()
 	if err != nil {
 		fmt.Printf("error loading gpt config: %v", err)
@@ -28,5 +73,10 @@ func CreateNewsBot() (NewsProvider, error) {
 	}
 
 	gptService := gpt.NewGPT(gptConfig, client)
-	return newNewsBot(gptService), nil
+
+	return &NewsBot{
+		TelegramBot: bot,
+		DB:          database,
+		GPTService:  gptService,
+	}, nil
 }
